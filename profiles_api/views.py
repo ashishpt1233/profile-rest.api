fdf07369b5d4d4cc4150384a2e5c3f31eade9d3d
apatel-sapiens <ashish.patel@sapiens.com>
2020-06-22 19:00:15 +0530

    enable views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        """ returns list API features"""
+        an_apiview = [
+            "Uses HTTP methods as function (get ,psot,patch,put,delete)",
+            "is similar to traditional django view",
+            "gives you the most control over application logic",
+            "is mapped manually to URLS",
+        ]
+
+        return Response ({"message":"Hello" ,"an_apiview": an_apiview})
