5008ce280f635d6199a121011c22b3330641ae23
apatel-sapiens <ashish.patel@sapiens.com>
2020-06-28 12:03:02 +0530

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,8 +8,10 @@ router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 router.register('profile',views.UserProfileViewSet)
 
+
 urlpatterns =[
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('',include(router.urls))
 
 ]
